3157be6042b71fbecd4067348aa7ce458033c2c4
Juan Lopera <juan.loperaa@udea.edu.co>
2023-08-25 12:38:00 -0500

    just a little modifications

---
+++ src/main/java/com/juanlopera/store/entities/Client.java
@@ -0,0 +1,57 @@
+package com.juanlopera.store.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Digits;
+
+@Entity
+@Table(name = "client")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 40)
+    private String name;
+
+    @Digits(integer = 10, fraction = 0)
+    @Column
+    private Long phoneNumber;
+
+    public Client(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    
+    
+    
+}
--- src/main/java/com/juanlopera/store/entities/Product.java
+++ src/main/java/com/juanlopera/store/entities/Product.java
@@ -14,8 +14,10 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private Float price;
+    
     @ManyToOne
     @JoinColumn(name = "category_id",nullable = true)
     private Category category;
