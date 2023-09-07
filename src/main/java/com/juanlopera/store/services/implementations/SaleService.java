06fcc53f0f80783ab19803e004977aaafcc098b0
Juan Lopera <juan.loperaa@udea.edu.co>
2023-09-06 21:18:23 -0500

    Creo lógica para descuentos en las ventas

--- src/main/java/com/juanlopera/store/services/implementations/SaleService.java
+++ src/main/java/com/juanlopera/store/services/implementations/SaleService.java
@@ -1,8 +1,11 @@
 package com.juanlopera.store.services.implementations;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Random;
+import java.util.Scanner;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,6 +20,7 @@ import com.juanlopera.store.repositories.contracts.ICustomerRepository;
 import com.juanlopera.store.repositories.contracts.IProductRepository;
 import com.juanlopera.store.repositories.contracts.ISaleRepository;
 import com.juanlopera.store.services.contracts.ISaleService;
+import com.juanlopera.store.util.Constantes;
 
 import jakarta.persistence.EntityNotFoundException;
 
@@ -45,11 +49,10 @@ public class SaleService implements ISaleService {
     public ResponseEntity<Sale> create(SaleRequestDTO saleRequest) {
         try {
             Sale sale = new Sale();
-
-            Customer customer = customerRepository.findById(saleRequest.getCustomerId()).orElseThrow(() -> new EntityNotFoundException("Cliente no encontrado"));
-
+            //Obtengo el cliente que está haciendo la compra
+            Customer customer = customerRepository.findById(saleRequest.getCustomerId()).orElseThrow(() -> new EntityNotFoundException("Cliente no encontrado, debes registrarlo primero"));
+            //Obtengo la lista de productos que van en la compra por medio de sus ids
             List<Product> products = productRepository.findAllById(saleRequest.getProductIds());
-
             if (products.isEmpty()){
                 throw new IllegalArgumentException("Debe haber al menos un producto");
             }
@@ -57,18 +60,46 @@ public class SaleService implements ISaleService {
             sale.setCustomer(customer);
             sale.setProducts(products);
 
+            //Calculo el total de la venta actual
+            Double totalActualPrice = 0d;
+            for (Product product : products) {
+                totalActualPrice += product.getPrice();
+            }
+            //Saco la lista de ventas que se le han realizado al cliente en los ultimos 31 días
+            List<Sale> oldSales = findByCustomerAndDateBetween(saleRequest.getCustomerId(), LocalDate.now().minus(31, ChronoUnit.DAYS), LocalDate.now()).getBody();
+            // Calculo el total de las ventas en los últimos 31 días
+            Double totalOldPrice = 0d;
+            for (Sale sale2 : oldSales) {
+                for (Product product : sale2.getProducts()) {
+                    totalOldPrice += product.getPrice(); 
+                }
+            }
+            // Aplico descuento si el cliente ha hecho compras mayores a un millón
+            if((totalActualPrice+totalOldPrice)>1000000){
+                System.out.println("Obtiene un descuento por compras mayores a un millón");
+                System.out.println("Valor a pagar: " + (totalActualPrice-(totalActualPrice*Constantes.DESCUENTO_CLIENTE_FIEL)));
+
+            //Llamo el juego para ver si hay descuento.
+            }else if(discountGame()){
+                System.out.println("Se ha ganado un descuento");
+                System.out.println("Valor a pagar: "+ (totalActualPrice-totalActualPrice*Constantes.DESCUENTO_JUEGO));
+            }else{
+                System.out.println("Valor a pagar: "+totalActualPrice);
+            }
+            
 
             return new ResponseEntity<Sale>(this.saleRepository.save(sale), HttpStatus.OK);
 
         } catch (EntityNotFoundException e) {
+            System.err.println("Se encontró una excepción: "+e.getMessage());
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
             
         }catch (IllegalArgumentException e){
+            System.err.println("Se encontró una excepción: "+e.getMessage());
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }catch (Exception e){
             return new ResponseEntity<Sale>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
     }
 
     @Override
@@ -97,10 +128,6 @@ public class SaleService implements ISaleService {
             Customer customer = new Customer();
             customer = this.customerRepository.findById(customerId).orElseThrow(()-> new EntityNotFoundException("El cliente no existe"));
             List<Sale> sales = customer.getSales();
-            // System.out.println("Hola---------------------------------------------------------------------------------------------------------------------------------------------------------------");
-            // Scanner scanner=new Scanner(System.in);
-            // scanner.nextLine();
-            // scanner.close();
             return new ResponseEntity<List<Sale>>(sales,HttpStatus.OK);
         }catch (EntityNotFoundException e){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
@@ -142,4 +169,31 @@ public class SaleService implements ISaleService {
         }
     }
     
+    private Boolean discountGame(){
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int numIntentos=1;
+        Boolean win=false;
+        do {
+            System.out.println("Preparado para ganar? Intento nro. "+numIntentos);
+            scanner.nextLine();
+            int option=random.nextInt(3)+1;
+            switch (option) {
+                case 1:
+                    win = true;
+                    return win;
+                case 2:
+                    numIntentos++;
+                    break;
+                case 3:
+                    System.out.println("No ganó el descuento");
+                    return win;
+                default:
+                    break;
+            }
+        } while (numIntentos<=3);        
+        scanner.close();
+        return win;
+
+    }
 }
---
+++ src/main/java/com/juanlopera/store/util/Constantes.java
@@ -0,0 +1,7 @@
+package com.juanlopera.store.util;
+
+public final class Constantes {
+    public static final float DESCUENTO_CLIENTE_FIEL = 0.1f;
+    public static final float DESCUENTO_JUEGO = 0.05f;
+    private Constantes(){};
+}
